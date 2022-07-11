205ab2fbf12c25d9e602b02c8b2a9af4dd7b4197
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-07-11 11:54:55 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function getNumberInfo() {
         txt += `Invalid Input.  Please enter a whole number between 2 and 6.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;     
-        txt += `Sum of the numbers is ${digitnPowers(num)}.`;
+        txt += `Sum of the numbers is .`;
       
     }
     
